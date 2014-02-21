2c99359c9c5c541667bd2d1ea312de96abe4c7f0
mocchi <boom.boom.planet@gmail.com>
2014-02-21 17:58:58 +0900

    show comment form

--- js/codeCommenter.js
+++ js/codeCommenter.js
@@ -1,5 +1,30 @@
 (function($) {
+    var LINE_ELEMENT = '<pre/>'
+
     $.fn.comment = function(){
+        var content = this;
+        var text = content.text();
+        var lines = text.split('\n');
+        content.html('');
+        content.addClass('code-commenter-content');
+        $.each(lines,function (){
+            /* set code */
+            var pre = $(LINE_ELEMENT);
+            pre.text(this == '' ? ' ':this);
+            /* comment line */
+            var commentline = $('<div/>');
+            //commentline.text(this);
+            pre.click(
+                function (){
+                    commentline.slideToggle();
+                }
+            );
+            content.append(pre);
+            content.append(commentline);
+        });
+        //console.log();
         return this;
     }
+
+    
 })(jQuery);
